c4de7ed80ec68c588d888c586c840492d22cc924
Cristian Padilla <cristian.padilla@dipzo.net>
2024-12-07 12:12:20 -0500

    Inject contact service

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,11 +3,13 @@ import { provideServerRendering } from '@angular/platform-server';
 import { provideServerRoutesConfig } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
+import { ContactService } from './services/contact.service';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    provideServerRoutesConfig(serverRoutes)
+    provideServerRoutesConfig(serverRoutes),
+    ContactService
   ],
 };
 
