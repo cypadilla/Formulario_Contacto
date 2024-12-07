698d19bb93048ef4848a8a2dab821f5201a806ce
Cristian Padilla <cristian.padilla@dipzo.net>
2024-12-07 13:40:38 -0500

    contact-form deploy

--- src/app/features/pages/contact-page/contact-page.component.ts
+++ src/app/features/pages/contact-page/contact-page.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-// import { ContactFormComponent } from '../../contact/contact-form/contact-form.component';
+import { ContactFormComponent } from '../../contact/contact-form/contact-form.component';
+import { User } from '../../../models/user.model';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
 
+  users: User[] = [];
   loading = true; // Indicador de carga
   error: string | null = null;
-  constructor(){}
+  constructor(){
+  }
 
 }
